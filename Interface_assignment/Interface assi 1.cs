743abcf1c148c3434a8f4d4126b0c37b1c8a20c4
pankajdaundkar <phd27993@gmail.com>
2023-06-23 12:57:22 +0530

    Add project files.

---
+++ Interface_assignment/Interface assi 1.cs
@@ -0,0 +1,124 @@
+﻿using Interface_assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Interface_assignment
+{
+    public interface Interface_assi_1
+    {
+        string PrintDetails();
+    }
+    public class emlpoyee : Interface_assi_1
+    {
+        public string name;
+        public int id;
+        public double bs, hra, ta, da, pf, gs, total;
+        public static int count;
+        public emlpoyee(string name, int bs)
+        {
+            count++;
+            this.name = name;
+            this.bs = bs;
+            this.id = count;
+        }
+        public static int Getcount()
+        {
+            return count;
+        }
+        public virtual void CalculateSalary()
+
+        {
+            hra = bs * 0.40;
+
+            ta = bs * 0.20;
+
+            da = bs * 0.15;
+
+            pf = bs * 0.12;
+
+            total = (bs + hra + ta + da);
+
+            gs = (total) - pf;
+
+        }
+        public override string ToString()
+        {
+            return $"Name is {name},\nId is {count},\nTotal salary is {total},\nGross salary is {gs}";
+        }
+
+        public string PrintDetails()
+        {
+
+            return $"Name is {name},\nId is {count},\nTotal salary is {total},\nGross salary is {gs}";
+        }
+    }
+    public class Sales_maneger : emlpoyee, Interface_assi_1
+    {
+        private double cumm;
+        public Sales_maneger(double cumm, int bs, string name) : base(name, bs)
+        {
+            this.cumm = cumm;
+        }
+        public override void CalculateSalary()
+
+        {
+            hra = bs * 0.40;
+
+            ta = bs * 0.20;
+
+            da = bs * 0.15;
+
+            pf = bs * 0.12;
+
+            total = (bs + hra + ta + da + cumm);
+
+            gs = (total) - pf;
+        }
+
+        public override string ToString()
+        {
+            return $"name is {name} \n id is {id}\ntotal salary is {total}\n gross salary is {gs}";
+        }
+        public string PrintDetails()
+        {
+
+            return $"Name is {name},\nId is {count},\nTotal salary is {total},\nGross salary is {gs}";
+        }
+    }
+    public class CEO : emlpoyee, Interface_assi_1
+    {
+        double bonus;
+        public CEO(double bonus, int bs, string name) : base(name, bs)
+        {
+            this.bonus = bonus;
+        }
+        public override void CalculateSalary()
+        {
+
+            hra = bs * 0.40;
+
+            ta = bs * 0.20;
+
+            da = bs * 0.15;
+
+            pf = bs * 0.12;
+
+            total = (bs + hra + ta + da + bonus);
+
+            gs = (total) - pf;
+        }
+        public override string ToString()
+        {
+            return $"name is {name},\n id is {id},\ntotal salary is {total},\ngross salary is {gs}";
+        }
+        public string PrintDetails()
+        {
+
+            return $"Name is {name},\nId is {count},\nTotal salary is {total},\nGross salary is {gs}";
+        }
+    }
+}
---
+++ Interface_assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            emlpoyee e1 =  new emlpoyee("pankaj",20000);
+            e1.CalculateSalary();
+            Console.WriteLine(e1.PrintDetails());
+
+            Sales_maneger s1 = new Sales_maneger(5000, 30000, "pratik");
+            s1.CalculateSalary();
+            Console.WriteLine(s1.PrintDetails());
+
+            CEO c1 = new CEO(2000, 30000, "sagar");
+            c1.CalculateSalary();
+            Console.WriteLine(c1.PrintDetails());   
+        }
+    }
+}
